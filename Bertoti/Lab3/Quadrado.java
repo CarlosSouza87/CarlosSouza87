ffb51e9de4f6a280cef5864832d3ae3a13d0a6d5
CarlosSouza87 <74521818+CarlosSouza87@users.noreply.github.com>
2022-06-08 22:08:24 -0300

    subindo
    subindo

---
+++ Bertoti/Lab3/Quadrado.java
@@ -0,0 +1,14 @@
+package geometricos;
+
+public class Quadrado {
+	public double l;
+	
+	
+	//Metodo - uma função que será utilizada na aplicação principal.
+	
+	public double area() {
+		double p = l;
+		return Math.pow(p, 2);
+}
+}
+	  
---
+++ Bertoti/Lab3/Retangulo.java
@@ -0,0 +1,14 @@
+package geometricos;
+
+public class Retangulo {
+	
+	public double d;
+	public double e;
+	
+	//Metodo - uma função que será utilizada na aplicação principal.
+	
+	public double area() {
+		double p = (d * e);
+		return (p);
+		}
+}
